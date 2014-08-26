a7a1a9db66837c14279383cb6e897d3260858c0f
jollaman999 <admin@jollaman999.com>
2014-08-27 02:30:35 +0900

    usb: serial: usb_wwan.c: Fix conflict

--- drivers/usb/serial/usb_wwan.c
+++ drivers/usb/serial/usb_wwan.c
@@ -884,23 +884,16 @@ int usb_wwan_resume(struct usb_serial *serial)
 
 		for (j = 0; j < N_IN_URB; j++) {
 			urb = portdata->in_urbs[j];
-
-			/* don't re-submit if it already was submitted or if
-			 * it is being processed by in_work */
-			if (urb->anchor || !list_empty(&urb->urb_list))
-				continue;
-
-			usb_anchor_urb(urb, &portdata->submitted);
 			err = usb_submit_urb(urb, GFP_ATOMIC);
 			if (err < 0) {
-				err("%s: Error %d for bulk URB[%d]:%p %d",
-				    __func__, err, j, urb, i);
-				usb_unanchor_urb(urb);
-				intfdata->suspended = 1;
+				err("%s: Error %d for bulk URB %d",
+				    __func__, err, i);
 				err_count++;
 			}
 		}
 	}
+	intfdata->suspended = 0;
+	spin_unlock_irq(&intfdata->susp_lock);
 
 	if (err_count)
 		return -EIO;
