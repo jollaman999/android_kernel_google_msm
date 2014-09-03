a3fb485ecfee7431a61832c5d9793500ea8d7a79
jollaman999 <admin@jollaman999.com>
2014-09-03 22:03:51 +0900

    msm: thermal: Adjust throttle

--- arch/arm/mach-msm/lge/mako/board-mako.c
+++ arch/arm/mach-msm/lge/mako/board-mako.c
@@ -1380,16 +1380,16 @@ static struct msm_thermal_data msm_thermal_pdata = {
 	.shutdown_temp = 80,
 
 	.allowed_max_high = 68,
-	.allowed_max_low = 61,
-	.allowed_max_freq = 810000,
+	.allowed_max_low = 56,
+	.allowed_max_freq = 918000,
 
-	.allowed_mid_high = 62,
-	.allowed_mid_low = 58,
-	.allowed_mid_freq = 918000,
+	.allowed_mid_high = 58,
+	.allowed_mid_low = 54,
+	.allowed_mid_freq = 1026000,
 
-	.allowed_low_high = 60,
-	.allowed_low_low = 56,
-	.allowed_low_freq = 1026000,
+	.allowed_low_high = 56,
+	.allowed_low_low = 50,
+	.allowed_low_freq = 1134000,
 };
 
 #define MSM_SHARED_RAM_PHYS 0x80000000
