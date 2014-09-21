d01eb5e0be77c3f2dbe23c701822ea335c21588f
Jaegeuk Kim <jaegeuk@kernel.org>
2014-09-20 21:57:51 -0700

    f2fs: introduce cp_control structure
    This patch add a new data structure to control checkpoint parameters.
    Currently, it presents the reason of checkpoint such as is_umount and normal
    sync.
    
    Reviewed-by: Chao Yu <chao2.yu@samsung.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    
    Conflicts:
    
    	fs/f2fs/recovery.c

--- fs/f2fs/recovery.c
+++ fs/f2fs/recovery.c
@@ -580,9 +580,11 @@ out:
 		set_ckpt_flags(sbi->ckpt, CP_ERROR_FLAG);
 		mutex_unlock(&sbi->cp_mutex);
 	} else if (need_writecp) {
-		f2fs_msg(sbi->sb, KERN_INFO, "recovery complete");
+		struct cp_control cpc = {
+			.reason = CP_SYNC,
+		};
 		mutex_unlock(&sbi->cp_mutex);
- 		write_checkpoint(sbi, false);
+		write_checkpoint(sbi, &cpc);
 	} else {
 		mutex_unlock(&sbi->cp_mutex);
 	}
