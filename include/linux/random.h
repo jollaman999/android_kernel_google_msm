d5bf1ff66608b02ee6c67229bf0a043d338a5d6e
jollaman999 <admin@jollaman999.com>
2014-11-18 23:27:57 +0900

    random: Fix build error

--- include/linux/random.h
+++ include/linux/random.h
@@ -115,6 +115,10 @@ static inline int arch_has_random(void)
 {
 	return 0;
 }
+static inline int arch_get_random_seed_long(unsigned long *v)
+{
+	return 0;
+}
 static inline int arch_has_random_seed(void)
 {
 	return 0;
